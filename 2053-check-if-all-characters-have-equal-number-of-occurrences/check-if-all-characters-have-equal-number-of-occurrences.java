f3e8dd62cdfd47db083a86ab99a432209efb6524
Anamikaaman <harshitaengi.127@gmail.com>
2025-07-07 19:36:21 +0530

    Time: 10 ms (14.91%) | Memory: 42.2 MB (47.73%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean areOccurrencesEqual(String s) {
         HashMap<Character, Integer> hm = new HashMap<>();
 
-        // Step 1: Count frequency of each character
+        
         for (int i = 0; i < s.length(); i++) {
             if (!hm.containsKey(s.charAt(i))) {
                 hm.put(s.charAt(i), 1);
@@ -13,14 +13,14 @@ class Solution {
             }
         }
 
-        // ✅ FIXED: Get any frequency from map, not s.charAt(0)
+        
         Integer ans = null;
         for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
             ans = entry.getValue();
             break;
         }
 
-        // Step 2: Check if all values are equal
+        
         for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
             if (!entry.getValue().equals(ans)) {
                 return false;
